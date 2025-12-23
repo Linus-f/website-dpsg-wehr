3fa0b09fc119b17af16e32d041c4e58ba0e7747a
Linus Fischer <mail@linus-fischer.de>
2025-12-23 23:30:14 +0100

    fix: downgrade vitest to ^0.34.6 for Vite 4 compatibility

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,7 +1,6 @@
 import { createClient } from 'tinacms/dist/client';
 import { queries } from './types';
 export const client = createClient({
-    cacheDir: '/Users/linus/code/website-dpsg-wehr/tina/__generated__/.cache/1766527422422',
     url: 'http://localhost:4001/graphql',
     token: 'undefined',
     queries,
